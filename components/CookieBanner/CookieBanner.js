2ac8b80b112c64d5b0d4b0d831a124c4a3b92b16
GuNShOtzZ <kevinloeding@outlook.de>
2024-03-18 10:35:47 +0100

    Add functionality to the cookie banner

--- components/CookieBanner/CookieBanner.js
+++ components/CookieBanner/CookieBanner.js
@@ -1,14 +1,61 @@
+import Cookies from "js-cookie";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function CookieBanner() {
+  const [isAccepted, setAccepted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    router.replace(router.asPath, undefined, { scroll: false });
+  }, [isAccepted]);
+
+  const handleAccept = () => {
+    setAccepted(true);
+    Cookies.set("cookieConsent", true, { expires: 365 });
+  };
+
+  const handleRemove = () => {
+    setAccepted(false);
+    Cookies.remove("cookieConsent", true, { expires: 365 });
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  if (isAccepted) {
+    return (
+      <div className=" bg-neutral-700 text-white p-4 flex flex-col gap-4">
+        <p>Cookies are currently consent on this website.</p>
+        <button
+          onClick={handleRemove}
+          className="px-2 py-1 bg-blue-500 text-white rounded w-fit"
+        >
+          Revoke permission
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className=" bg-neutral-700 text-white p-4 flex flex-col gap-4">
       <p>
         This website uses cookies. By using this website, you consent to the use
         of cookies.
       </p>
-      <div className="flex gap-4">
-        <button className="px-2 py-1 bg-blue-500 rounded">Zustimmen</button>
-        <button className="px-2 py-1 bg-blue-500 rounded">Ablehnen</button>
-      </div>
+      <button
+        onClick={handleAccept}
+        className="px-2 py-1 bg-blue-500 rounded w-fit"
+      >
+        Allow cookies
+      </button>
     </div>
   );
 }
